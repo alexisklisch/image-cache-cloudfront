a9343f1b2027725baf0bafaefd481d563e7496eb
Alexis Fleitas Klisch <unbosquense@gmail.com>
2025-11-13 16:32:50 +0000

    Refactor build-script to exclude 'dependencies' from package.json output

--- build-script.ts
+++ build-script.ts
@@ -2,6 +2,6 @@ import { writeFile } from 'node:fs/promises'
 // package.json
 import packageJson from './package.json' with { type: 'json' }
 
-const sameSection = ['name', 'type', 'version', 'author', 'license', 'dependencies']
+const sameSection = ['name', 'type', 'version', 'author', 'license']
 const newPackageJson = Object.fromEntries(Object.entries(packageJson).filter(([key]) => sameSection.includes(key)))
 await writeFile('dist/package.json', JSON.stringify({ ...newPackageJson, main: 'index.js' }, null, 2))
